d91213164aefe78e373f22aaabaa1b748c743e43
Adam <adamwberck@gmail.com>
2019-04-18 00:37:12 -0400

    Data and File Info is written correctly.

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 #include "my_stack.h"
 
 #define FREE_BLOCK   0x0000
@@ -57,6 +58,7 @@ struct boot{
 };
 
 
+void write_data(void *disk,char *data,unsigned short size,unsigned short fat_loc);
 void create_file(void *disk,struct dir_entry parent, char name[NAME_LENGTH],char ext[EXT_LENGTH],
                  char *data,unsigned short size);
 void write_file_to_fat(struct dir_entry entry,void *disk);
@@ -73,37 +75,35 @@ void print_mapped_data(unsigned char *mapped_data);
 unsigned short get_free_block(void *disk,unsigned short start);
 
 int main(){
-    FILE* disk = fopen("my_disk","w+");
+    FILE* new_disk = fopen("my_disk","w+");
     unsigned short int empty = FREE_BLOCK;
     for(int i=0;i<TOTAL_SIZE;i++) {
-        fwrite(&empty, sizeof(unsigned short int), 1, disk);
+        fwrite(&empty, sizeof(unsigned short int), 1, new_disk);
     }
-    fseek(disk,0,SEEK_SET);
+    fseek(new_disk,0,SEEK_SET);
     struct boot my_boot = create_boot();
-    fwrite(my_boot.valid_check,1, sizeof(my_boot.valid_check),disk);
-    fwrite(&my_boot.boot_sector_size,2, 1,disk);
-    fwrite(&my_boot.total_size,4, 1,disk);
-    fwrite(&my_boot.block_size,2, 1,disk);
-    fwrite(&my_boot.total_blocks,2, 1,disk);
-    fwrite(&my_boot.FAT_size,2, 1,disk);
-    fwrite(&my_boot.max_size,2, 1,disk);
-    //write_dir_entry(my_boot.root,disk);
-    fclose(disk);
+    fwrite(my_boot.valid_check,1, sizeof(my_boot.valid_check),new_disk);
+    fwrite(&my_boot.boot_sector_size,2, 1,new_disk);
+    fwrite(&my_boot.total_size,4, 1,new_disk);
+    fwrite(&my_boot.block_size,2, 1,new_disk);
+    fwrite(&my_boot.total_blocks,2, 1,new_disk);
+    fwrite(&my_boot.FAT_size,2, 1,new_disk);
+    fwrite(&my_boot.max_size,2, 1,new_disk);
+    //write_dir_entry(my_boot.root,new_disk);
+    fclose(new_disk);
 
     int i_disk = open("my_disk",O_RDWR,0);
-    void *mapped_data = mmap(NULL,TOTAL_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,i_disk,0);
-    write_dir_entry(my_boot.root,mapped_data,ROOT_LOCATION);
-    write_file_to_fat(my_boot.root,mapped_data);
+    void *disk = mmap(NULL,TOTAL_SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,i_disk,0);
+    write_dir_entry(my_boot.root,disk,ROOT_LOCATION);
+    write_file_to_fat(my_boot.root,disk);
 
     char *data = "Hello this is a test file.\0";
     create_file(disk,my_boot.root,"test\0","txt",data,27);
 
 
-
-
-    void *p = mapped_data+FAT2_LOCATION;
-    unsigned short d = 0xABCD;
-    memcpy(p,&d,2);
+    //void *p = disk+USER_SPACE_LOCATION;
+    //unsigned short d = 0xABCD;
+    //memcpy(p,&d,2);
     //print_mapped_data(mapped_data);
 }
 
@@ -116,12 +116,13 @@ void print_mapped_data(unsigned char *mapped_data) {
     }
 }
 
+//TODO check duplicate file name
 unsigned int get_dir_location(void *disk,struct dir_entry parent){
     int temp_size = parent.size;
     unsigned short fat = parent.FAT_location;
-    while(temp_size>512){
+    while(temp_size>BLOCK_SIZE){
         fat = fat_value(disk,fat);
-        temp_size-=512;
+        temp_size-=BLOCK_SIZE;
     }
     return (unsigned int) (USER_SPACE_LOCATION + fat * BLOCK_SIZE + temp_size);
 }
@@ -130,11 +131,26 @@ unsigned int get_dir_location(void *disk,struct dir_entry parent){
 void create_file(void *disk,struct dir_entry parent, char name[NAME_LENGTH],char ext[EXT_LENGTH],
         char *data,unsigned short size){
     time_t the_time = time(NULL);
-    unsigned short loc = get_free_block(disk,0x0001);
-    struct dir_entry entry = create_entry(name,ext,size,the_time,the_time,loc);
-    unsigned int location = get_dir_location(disk,parent);
-    printf("location %x\n",location);
-    //write_dir_entry(entry,disk,)
+    unsigned short fat_loc = get_free_block(disk,0x0000);
+    struct dir_entry entry = create_entry(name,ext,size,the_time,the_time,fat_loc);
+    unsigned int disk_loc = get_dir_location(disk,parent);
+    //printf("disk_loc %x\n",disk_loc);
+    write_dir_entry(entry,disk,disk_loc);
+    write_file_to_fat(entry,disk);
+    write_data(disk,data,size,fat_loc);
+}
+
+void write_data(void *disk,char *data,unsigned short size,unsigned short fat_loc){
+    char block_of_data[BLOCK_SIZE];
+    int temp_size = size;
+    int disk_loc = USER_SPACE_LOCATION+fat_loc*BLOCK_SIZE;
+    while(temp_size>0){
+        memcpy(block_of_data,data,BLOCK_SIZE);
+        memcpy(disk+disk_loc,block_of_data,BLOCK_SIZE);
+
+        fat_loc=fat_value(disk,fat_loc);
+        temp_size -= BLOCK_SIZE;
+    }
 }
 
 void write_file_to_fat(struct dir_entry entry,void *disk){
